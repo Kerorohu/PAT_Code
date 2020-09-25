21929b2b1f5e5c22d8d925dee7927780c5ff3e0e
yixuan hu <hyx_4@163.com>
2020-09-25 17:01:27 +0800

    youwenti

--- 1020.cpp
+++ 1020.cpp
@@ -1,19 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <vector>
 using namespace std;
-
 bool cmp(int a, int b);
-
 int main() {
-	int zhonglei, zuidaxuqiu, zuidashouyi, yuebingshuliang;
+	int zhonglei, zuidaxuqiu,zuidashouyi,yuebingshuliang;
 	int* kucun = new int[zhonglei];
 	int* zongshoujia = new int[zhonglei];
 	int* danjia = new int[zhonglei];
 	vector<pair<int, int>> kczsj;
-
+	
 	yuebingshuliang = 0;
 	cin >> zhonglei >> zuidaxuqiu;
 
@@ -22,27 +20,26 @@ int main() {
 	}
 	for (int i = 0; i < zhonglei; i++) {
 		cin >> zongshoujia[i];
-		danjia[i] = (zongshoujia[i] * 1000) / kucun[i];
+		danjia[i] = (zongshoujia[i] * 1000) / kucun;
 		kczsj.push_back(pair<int, int>(kucun[i], danjia[i]));
 	}
 
-	sort(kczsj.begin(), kczsj.end(), cmp);
+	sort(kczsj.begin(), kczsj.end(),cmp);
 	int n = 0;
 	zuidashouyi = 0;
 	vector<pair<int, int>>::iterator it;
 	it = kczsj.begin();
 
 	while (yuebingshuliang < zuidaxuqiu) {
-		n = (zuidaxuqiu - yuebingshuliang) / (*it->second);
-
+		n = (zuidaxuqiu - yuebingshuliang) / *it->second;
+		
 		if (n > (zuidaxuqiu - yuebingshuliang))
 		{
 			n = zuidaxuqiu - yuebingshuliang;
-		}
-		else if (n > (*it->first)) {
+		}else if (n > (*it->first)){
 			n = (*it->first);
 		}
-		zuidashouyi += n * (*it->second);
+		zuidashouyi += n * (* it->second);
 		yuebingshuliang += n;
 		it++;
 	}
@@ -50,6 +47,6 @@ int main() {
 	cout << yuebingshuliang;
 }
 
-bool cmp(pair<int, int> a, pair<int, int> b) {
+bool cmp(pair<int,int> a ,pair<int,int> b) {
 	return a.second > b.second;
 }
