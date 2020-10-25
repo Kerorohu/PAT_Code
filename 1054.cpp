55cf829ec3259ebcdc9b85add18b7cfd3b94f7ba
hu yixuan <keroro2020@163.com>
2020-10-25 11:14:30 +0800

    1054

--- 1054.cpp
+++ 1054.cpp
@@ -4,22 +4,25 @@
 using namespace std;
 
 int main() {
-	int num,cn = 0;
+	int num, cn = 0;
 	cin >> num;
-	string temp,sub;
-	float number,sum = 0.0;
+	string temp, sub;
+	float number, sum = 0.0;
 	for (int i = 0; i < num; i++) {
 		cin >> temp;
 		try
 		{
 			number = stof(temp);
-			if (temp.find('.') == string::npos) {
+			if (number > 1000 || number < -1000) {
+				cout << "ERROR: " << temp << " is not a legal number" << endl;
+			}
+			else if (temp.find('.') == string::npos) {
 				sum += number;
 				cn++;
 			}
 			else
 			{
-				sub = temp.substr(temp.find('.') + 1, temp.end());
+				sub = temp.substr(temp.find('.') + 1);
 				if (sub.size() <= 2) {
 					sum += number;
 					cn++;
@@ -29,15 +32,18 @@ int main() {
 					cout << "ERROR: " << temp << " is not a legal number" << endl;
 				}
 			}
-			
+
 		}
-		catch (const std::exception &e)
+		catch (const std::exception&)
 		{
 			cout << "ERROR: " << temp << " is not a legal number" << endl;
-		}	
+		}
 	}
 	if (cn == 0) {
 		cout << "The average of 0 numbers is Undefined";
+	}
+	else if (cn == 1) {
+		cout << "The average of 1 number is " << fixed << setprecision(2) << sum;
 	}
 	else
 	{
